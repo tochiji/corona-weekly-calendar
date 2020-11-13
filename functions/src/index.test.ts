a1d5c09fabb3d405788543d0ec2c8a071631145d
Yosi <7910558+tochiji@users.noreply.github.com>
2020-11-13 17:27:17 +0900

    feat: update test

--- functions/src/index.test.ts
+++ functions/src/index.test.ts
@@ -7,12 +7,12 @@ test('指定のkeyが存在しているか？', async () => {
   expect(data[0]['都道府県名']).not.toBeUndefined();
   expect(data[0]['市区町村名']).not.toBeUndefined();
   expect(data[0]['公表_年月日']).not.toBeUndefined();
-  expect(data[0]['曜日']).not.toBeUndefined();
   expect(data[0]['発症_年月日']).not.toBeUndefined();
+  expect(data[0]['確定_年月日']).not.toBeUndefined();
   expect(data[0]['患者_居住地']).not.toBeUndefined();
   expect(data[0]['患者_年代']).not.toBeUndefined();
   expect(data[0]['患者_性別']).not.toBeUndefined();
-  expect(data[0]['患者_属性']).not.toBeUndefined();
+  expect(data[0]['患者_職業']).not.toBeUndefined();
   expect(data[0]['患者_状態']).not.toBeUndefined();
   expect(data[0]['患者_症状']).not.toBeUndefined();
   expect(data[0]['患者_渡航歴の有無フラグ']).not.toBeUndefined();
